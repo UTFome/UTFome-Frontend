c2261d5cb212a681a6aa5fc25213cd174b0b2f5e
Felipe Archanjo <felipecunhamendes@gmail.com>
2022-04-02 16:48:23 -0300

    atualizações

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,9 +1,26 @@
 import styles from './Header.module.scss';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function Header(){
     return (
-        <>
-            
-        </>
+        <Router>
+            <header className='header'>
+
+                <a href='#' className='logo'>
+                    LOGO
+                </a>
+
+                <nav className='navbar'>
+                    <a href='#home'>home</a>
+                    <a href='#produtos'>produtos</a>
+                    <a href='#sobre'>sobre</a>
+                    <a href='#contato'>contato</a>
+                    <a href='#conta'>conta</a>
+                </nav>
+
+                <
+
+            </header>
+        </Router>
     );
 }
