eff95239d42673f8033e39db5fd0edf7ee589950
Pamella Tamura <pamella.tamura@mznsolucoes.com.br>
2022-04-03 18:18:44 -0300

    UTFOME-002: modificando footer e atualizando branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// import Footer from './components/layout/footer/Footer';
+import Footer from './components/layout/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 }
--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,42 +1,50 @@
 import "./Footer.css";
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin, FaPinterest } from 'react-icons/fa';
+import {  FaFacebookF,  FaTwitter,  FaInstagram,  FaLinkedin,  FaPinterest,} from "react-icons/fa";
+import Bee from "../../../assets/images/bee_pequeno.png";
 
 function Footer() {
   return (
     <section className="footer">
+      <div className="col-md-6">
+        <div>Info Info Info Info</div>
+        <div>Info Info Info Info</div>
+      </div>
 
-    <div className="share">
+      <div className="col-md-6">
+        <div className="links">
+          <a href="#">home</a>
+          <a href="#">about</a>
+          <a href="#">menu</a>
+          <a href="#">products</a>
+          <a href="#">review</a>
+          <a href="#">conta</a>
+          <a href="#">blogs</a>
+        </div>
+      </div>
+      <img className="logo-bee" src={Bee} alt="Bee" />
+
+      <div className="share">
         <a href="#">
-            <FaFacebookF className="icone-facebook"/>
+          <FaFacebookF className="icone-facebook" />
         </a>
         <a href="#">
-            <FaTwitter className="icone-twitter"/>
+          <FaTwitter className="icone-twitter" />
         </a>
         <a href="#">
-            <FaInstagram className="icone-instagram"/>
+          <FaInstagram className="icone-instagram" />
         </a>
         <a href="#">
-            <FaLinkedin className="icone-linkedin"/>
+          <FaLinkedin className="icone-linkedin" />
         </a>
         <a href="#">
-            <FaPinterest className="icone-pinterest"/>
+          <FaPinterest className="icone-pinterest" />
         </a>
-    </div>
+      </div>
 
-    <div className="links">
-        <a href="#">home</a>
-        <a href="#">about</a>
-        <a href="#">menu</a>
-        <a href="#">products</a>
-        <a href="#">review</a>
-        <a href="#">contact</a>
-        <a href="#">blogs</a>
-    </div>
-
-    <div className="credit">created by <span>UTFome</span> | all rights reserved</div>
-
-</section>
-
+      <div className="credit">
+        created by <span>UTFome</span> | all rights reserved
+      </div>
+    </section>
   );
 }
 
