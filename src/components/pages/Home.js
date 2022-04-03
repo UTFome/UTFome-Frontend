11cd4a7a73c56327231269738be5668fe3d54859
Felipe Archanjo <felipecunhamendes@gmail.com>
2022-04-03 17:36:37 -0300

    detalhes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,14 +4,14 @@ import './Home.css';
 function Home(){
     return (
         <section className="home" id="home">
-        <div className="content">
-            <h3>UTFOME</h3>
-            <p>Não se preocupe em encontrar algo para comer, nós temos tudo o que você precisa pertinho de você!</p>
-            <a href="#" className="btn botao-entrar">BEE HAPPY</a>
-        </div>
+            <div className="content">
+                <h3>UTFOME</h3>
+                <p>Não se preocupe em encontrar algo para comer, nós temos tudo o que você precisa pertinho de você!</p>
+                <a href="#" className="btn botao-entrar">BEE HAPPY</a>
+            </div>
     
-    </section>
-    )
+        </section>
+    );
 
 }
 export default Home;
