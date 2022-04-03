e9487eac3ed8ea00c88677513103325ea6404eca
Felipe Archanjo <felipecunhamendes@gmail.com>
2022-04-03 17:32:55 -0300

    Classname

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-//import Footer from './components/layout/footer/Footer';
+import Footer from './components/layout/footer/Footer';
 //import Header from './components/layout/header/Header';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <Footer />
     </div>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,11 @@ import './Home.css';
 
 function Home(){
     return (
-        <section class="home" id="home">
-        <div class="content">
+        <section className="home" id="home">
+        <div className="content">
             <h3>UTFOME</h3>
             <p>Não se preocupe em encontrar algo para comer, nós temos tudo o que você precisa pertinho de você!</p>
-            <a href="#" class="btn botao-entrar">BEE HAPPY</a>
+            <a href="#" className="btn botao-entrar">BEE HAPPY</a>
         </div>
     
     </section>
