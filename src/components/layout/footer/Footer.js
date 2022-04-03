e2ff5c4644badf085088441f3d77836f5d615533
Felipe Archanjo <felipecunhamendes@gmail.com>
2022-04-03 17:53:25 -0300

    Arrumando icones do footer

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -6,21 +6,11 @@ function Footer() {
     <section className="footer">
 
     <div className="share">
-        <a href="#">
-            <FaFacebookF className="icone-facebook"/>
-        </a>
-        <a href="#">
-            <FaTwitter className="icone-twitter"/>
-        </a>
-        <a href="#">
-            <FaInstagram className="icone-instagram"/>
-        </a>
-        <a href="#">
-            <FaLinkedin className="icone-linkedin"/>
-        </a>
-        <a href="#">
-            <FaPinterest className="icone-pinterest"/>
-        </a>
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-pinterest"></a>
     </div>
 
     <div className="links">
