0c5e6e4e55053f22f1d504233cdc1b0eb2c06662
Felipe Archanjo <felipecunhamendes@gmail.com>
2022-04-03 22:42:13 -0300

    UTFOME-035: Criando a pagina de contato

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Footer from './components/layout/footer/Footer';
 import Header from './components/layout/header/Header';
 import About from './components/pages/about/About';
 import Home from './components/pages/home/Home';
+import Contact from './components/pages/contact/Contact';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <Contact />
     </div>
   );
 }
