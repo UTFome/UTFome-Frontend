6a3cd916ea85bff3c0f2a98af033bf190b12d66f
Pamella Tamura <pamella.tamura@mznsolucoes.com.br>
2022-04-03 22:49:01 -0300

    UTFOME-033: desenvolvendo section de reviews

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Footer from './components/layout/footer/Footer';
 import Header from './components/layout/header/Header';
 import About from './components/pages/about/About';
+import Review from './components/pages/review/Review';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
-      <About />
     </div>
   );
 }
