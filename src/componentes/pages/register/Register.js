3c8c13b1188f491a88505444182830eef187af10
Marcos Rampaso <marcosbezner@gmail.com>
2022-05-12 03:40:14 -0300

    Refiz todo o formulário para ser compatível com o formik, tá indo tudo bem até agora, possui até a validação para o email institucional, agora eu to só aperfeiçoando

--- src/componentes/pages/register/Register.js
+++ src/componentes/pages/register/Register.js
@@ -3,20 +3,57 @@ import './Register.css';
 import logo from '../../../assets/images/Logo_grande.png';
 import { useState, useContext } from 'react';
 import { AuthContext } from '../../../contexts/auth';
+import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
 
-const Register = () => {
-    const { register } = useContext(AuthContext);
 
-    const [name, setName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const initialValues = {
+    name: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+const onSubmit = values => {
+    console.log('form data', values)
+}
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Submitted", { name, lastName, email, password });
-        register(name, lastName, email, password);
+const validate = values => {
+
+
+    let errors = {
+
     }
+
+    if (!values.name) {
+        errors.name = 'Obrigatório!'
+    }
+    if (!values.lastName) {
+        errors.lastName = 'Obrigatório!'
+    }
+    if (!values.email) {
+        errors.email = 'Obrigatório!'
+    }else if(!/^[A-Z0-9._%+-]+@[utfpr.edu.]+\.[B-R]{2,4}$/i.test(values.email)){
+        errors.email = 'email inválido! (utilize seu email institucional)'
+    }
+    if (!values.password) {
+        errors.password = 'Obrigatório!'
+    }
+    if (!values.confirmPassword) {
+        errors.confirmPassword = 'Obrigatório'
+    }
+    return errors
+}
+
+
+function Register() {
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+    })
+
+    console.log('form errors', formik.touched)
 
     return (
         <div className='container'>
@@ -24,93 +61,105 @@ const Register = () => {
                 <div className='wrap-register'>
                     <form
                         className='login-form'
-                        onSubmit={handleSubmit}>
+                        onSubmit={formik.handleSubmit}>
                         <span className='login-form-title'>
-                            <img src={logo}></img>
+                            <Link to="/main">
+                                <img src={logo}></img>
+                            </Link>
                         </span>
-                
-                        <div className='wrap-input'>
+
+                        <div className='form-control'>
+
                             <input
-                                className={name !== '' ? 'has-val input' : 'input'}
-                                type="text"
-                                value={name}
-                                onChange={e => setName(e.target.value)} />
+                                type='text'
+                                id='name'
+                                name='name'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.name}
+                            />
                             <span className='focus-input' data-placeholder="Nome"></span>
-                            <i class="fas fa-check-circle iconeVerdadeiro"></i>
-                            <i class="fas fa-exclamation-circle iconeFalso"></i>
-                            <small class = "errorMessage">Mensagem de erro</small>
+                            { formik.touched.name && formik.errors.name ? (
+                            <div className='error'>{formik.errors.name}
+                            </div>) : null}
                         </div>
 
-                        <div className='wrap-input'>
+                        <div className='form-control'>
+
                             <input
-                                className={lastName !== '' ? 'has-val input' : 'input'}
-                                type="text"
-                                value={lastName}
-                                onChange={e => setLastName(e.target.value)} />
+                                type='text'
+                                id='lastName'
+                                name='lastName'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.lastName}
+                            />
                             <span className='focus-input' data-placeholder="Sobrenome"></span>
-                            <i class="fas fa-check-circle iconeVerdadeiro"></i>
-                            <i class="fas fa-exclamation-circle iconeFalso"></i>
-                            <small class = "errorMessage">Mensagem de erro</small>
+                            { formik.touched.lastName && formik.errors.lastName ? (
+                            <div className='error'>{formik.errors.lastName}
+                            </div>) : null}
                         </div>
 
-                        <div className='wrap-input'>
+                        <div className='form-control'>
+
                             <input
-                                className={email !== '' ? 'has-val input' : 'input'}
-                                type="email"
-                                value={email}
-                                onChange={e => setEmail(e.target.value)} />
+                                type='email'
+                                id='email'
+                                name='email'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.email}
+                            />
                             <span className='focus-input' data-placeholder="Email"></span>
-                            <i class="fas fa-check-circle iconeVerdadeiro"></i>
-                            <i class="fas fa-exclamation-circle iconeFalso"></i>
-                            <small class = "errorMessage">Mensagem de erro</small>
+                            {formik.touched.email && formik.errors.email ? (
+                            <div className='error'>{formik.errors.email}
+                            </div>) : null}
                         </div>
 
-                        <div className='wrap-input'>
+                        <div className='form-control'>
+
                             <input
-                                className={password !== '' ? 'has-val input' : 'input'}
-                                type="password"
-                                value={password}
-                                onChange={e => setPassword(e.target.value)} />
+                                type='password'
+                                id='password'
+                                name='password'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.password}
+                            />
                             <span className='focus-input' data-placeholder="Senha"></span>
-                            <i class="fas fa-check-circle iconeVerdadeiro"></i>
-                            <i class="fas fa-exclamation-circle iconeFalso"></i>
-                            <small class = "errorMessage">Mensagem de erro</small>
+                            {formik.touched.password && formik.errors.password ? (
+                            <div className='error'>{formik.errors.password}
+                            </div>) : null}
                         </div>
 
-                        <div className='wrap-input'>
+                        <div className='form-control'>
+
                             <input
-                                className={password !== '' ? 'has-val input' : 'input'}
-                                type="confirmPassword"
-                                value={password}
-                                onChange={e => setPassword(e.target.value)} />
+                                type='password'
+                                id='confirmPassword'
+                                name='confirmPassword'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.confirmPassword}
+                            />
                             <span className='focus-input' data-placeholder="Confirme sua senha"></span>
-                            <i class="fas fa-check-circle iconeVerdadeiro"></i>
-                            <i class="fas fa-exclamation-circle iconeFalso"></i>
-                            <small class = "errorMessage">Mensagem de erro</small>
+                            {formik.touched.confirmPassword  && formik.errors.confirmPassword ? (
+                            <div className='error'>{formik.errors.confirmPassword}
+                            </div>) : null}
                         </div>
 
-                        <div className='container-login-form-btn'>
-                            <button className='login-form-btn'>
-                                Cadastre-se
-                            </button>
-                        </div>
 
-                        <div className='text-center'>
-                            <span className='txt1'>
-                                Já possui uma conta?
-                            </span>
-
-                            <a href='#' className='txt2'>
-                                Faça login
-                            </a>
-                        </div>
+                        <button className='login-form-btn'
+                            type='submit'>              
+                            Cadastre-se
+                        </button>
                     </form>
                 </div>
             </div>
         </div>
     )
-}
 
+}
 
 
 export default Register;
