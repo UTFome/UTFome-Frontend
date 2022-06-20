84dd52c7167d623d9a8ff27a7c0995fad3c28791
Pamella Tamura <pamella.tamura@mznsolucoes.com.br>
2022-06-20 15:25:19 -0300

    UTFOME-98: desenvolvendo modal

--- src/componentes/pages/main/main.js
+++ src/componentes/pages/main/main.js
@@ -6,6 +6,7 @@ import Review from '../review/Review';
 import Produto1 from '../../../assets/images/bee_redondo_pequeno.png';
 import Produto2 from '../../../assets/images/bee_redondo_pequeno.png';
 import Produto3 from '../../../assets/images/bee_redondo_pequeno.png';
+import ProdutoModal1 from '../../../assets/images/comida2.jpg';
 import Modal from 'react-modal';
 import { useState } from 'react';
 
@@ -49,11 +50,28 @@ function Main(){
                         <img src={Produto1}></img>
                         <h3>tasty and healty</h3>
                         <div className="price">$15.99 <span>20.99</span></div>
-                        <div class="icons">
-                            <button class="modal-button fas fa-eye a" onClick={handleOpenModal} ></button>
+                        <div className="icons">
+                            <button className="modal-button fas fa-eye a" onClick={handleOpenModal} ></button>
                             <Modal isOpen={modalIsOpen} onRequestClose={handleCloseModal}>
-                            <h2> AQUI VAI O CONTEUDO DE CADA PRODUTO! </h2>
-                            <button onClick={handleCloseModal}> Fechar </button>
+                                <div className="modal-topo">
+                                    <button onClick={handleCloseModal}> x </button>
+                                    {/* <h2> DESCRIÇÃO DO PRODUTO </h2> */}
+                                    <div className="blogs" id="blogs">
+                                        <h1 className="heading"> our <span>DESCRIÇÃO DO PRODUTO</span> </h1>
+                                        <div className="box-container">
+                                            <div className="box">
+                                                <div className="image">
+                                                    <img src={ProdutoModal1}></img>
+                                                </div>
+                                                <div className="content">
+                                                    <a href="#" className="title">tasty and refreshing coffee</a>
+                                                    <span>by admin / 21st may, 2021</span>
+                                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                                </div>
                             </Modal>
                         </div>
                     </div>
@@ -62,8 +80,8 @@ function Main(){
                         <img src={Produto2}></img>
                         <h3>tasty and healty</h3>
                         <div className="price">$15.99 <span>20.99</span></div>
-                        <div class="icons">
-                            <button href="#" class="fas fa-eye a" ></button>
+                        <div className="icons">
+                            <button href="#" className="fas fa-eye a" ></button>
                         </div>
                     </div>
 
@@ -71,8 +89,8 @@ function Main(){
                         <img src={Produto3}></img>
                         <h3>tasty and healty</h3>
                         <div className="price">$15.99 <span>20.99</span></div>
-                        <div class="icons">
-                        <button href="#" class="fas fa-eye a" ></button>
+                        <div className="icons">
+                        <button href="#" className="fas fa-eye a" ></button>
                         </div>
                     </div>
 
@@ -80,8 +98,8 @@ function Main(){
                         <img src={Produto3}></img>
                         <h3>tasty and healty</h3>
                         <div className="price">$15.99 <span>20.99</span></div>
-                        <div class="icons">
-                            <button href="#" class="fas fa-eye a" ></button>
+                        <div className="icons">
+                            <button href="#" className="fas fa-eye a" ></button>
                         </div>
                     </div>
 
@@ -89,8 +107,8 @@ function Main(){
                         <img src={Produto3}></img>
                         <h3>tasty and healty</h3>
                         <div className="price">$15.99 <span>20.99</span></div>
-                        <div class="icons">
-                            <button href="#" class="fas fa-eye a" ></button>
+                        <div className="icons">
+                            <button href="#" className="fas fa-eye a" ></button>
                         </div>
                     </div>
 
@@ -98,8 +116,8 @@ function Main(){
                         <img src={Produto3}></img>
                         <h3>tasty and healty</h3>
                         <div className="price">$15.99 <span>20.99</span></div>
-                        <div class="icons">
-                            <button href="#" class="fas fa-eye a" ></button>
+                        <div className="icons">
+                            <button href="#" className="fas fa-eye a" ></button>
                         </div>
                     </div>
 
