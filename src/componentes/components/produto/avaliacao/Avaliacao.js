5c556778e33af4fbfef84433a46dcfe4a36b4d53
Pamella Tamura <pamella.tamura@mznsolucoes.com.br>
2022-06-20 14:35:41 -0300

    UTFOME-97: componentizando review

---
+++ src/componentes/components/produto/avaliacao/Avaliacao.js
@@ -0,0 +1,34 @@
+import "./Avaliacao.css";
+import Bee from "../../../../assets/images/bee_redondo_pequeno.png";
+import Aspas from "../../../../assets/images/aspas_pequena.png";
+
+function Avaliacao() {
+    return(
+        <div className="review" id="review">
+            <div className="box-container">
+                <div className="box">
+                    <div className="aspas">
+                        <img src={Aspas} />
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nulla sit libero nemo fuga sequi nobis? Necessitatibus aut laborum, nisi quas eaque laudantium consequuntur iste ex aliquam minus vel? Nemo.</p>
+                    
+                    <div className="bee">
+                        <img src={Bee} />
+                    </div>
+                    
+                    <h3>Pessoa 1</h3>
+
+                    <div className="stars">
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export default Avaliacao;
