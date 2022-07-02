7d3cdf6833fcc2ce9049ae7df856a52b141842ba
Pamella Tamura <pamella.tamura@mznsolucoes.com.br>
2022-07-02 02:28:20 -0300

    UTFOME-111: desenvolvendo carousel de imagem

--- src/componentes/pages/main/main.js
+++ src/componentes/pages/main/main.js
@@ -4,33 +4,50 @@ import Header from "../../layout/header/Header";
 import Footer from "../../layout/footer/Footer";
 import Review from "../review/Review";
 import Produto1 from "../../../assets/images/bee_redondo_pequeno.png";
-import Produto2 from "../../../assets/images/bee_redondo_pequeno.png";
-import Produto3 from "../../../assets/images/bee_redondo_pequeno.png";
+import Banner1 from "../../../assets/images/comida3.jpg";
+import Banner2 from "../../../assets/images/comida4.jpg";
 import ProdutoModal1 from "../../../assets/images/comida2.jpg";
 import Modal from "react-modal";
 import { useState } from "react";
 import { listProducts } from "../../../services/api";
+import ReactElasticCarousel from "react-elastic-carousel";
+import Item from "../../components/produto/item/Item";
 
 Modal.setAppElement("#root");
 
 function Main() {
     const [modalIsOpen, setIsOpen] = useState(false);
     const [products, setVal] = useState([]);
+
+    /******** Carousel ********/
     const breakPoints = [
-        { width: 1, itemToShow: 1},
-        { width: 550, itemToShow: 2, itemScroll: 2},
-        { width: 768, itemToShow: 3},
-        { width: 1200, itemToShow: 4},
-    ]
-    const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8])
+        { width: 1, itemToShow: 1 },
+        { width: 550, itemToShow: 2, itemScroll: 2 },
+        { width: 768, itemToShow: 3 },
+        { width: 1200, itemToShow: 4 },
+    ];
+    const [items, setItems] = useState([
+        <div className="content banner-food">
+            <h3 className="titulo">Qual será a sua escolha?</h3>
+            <p className="subtitulo">
+            Você não precisa ir longe e nem esperar horas por uma comida,
+            escolha uma opção pertinho de você
+        </p>
+        </div>,
+        <img src={Banner2} className="banner-comida" />,
+        <img src={Banner1} className="banner-comida" />,
+        <img src={Banner2} className="banner-comida" />,
+    ]);
+
     const addItem = () => {
-        const nextItem = Math.max(1, items.length + 1)
-        setItems([...items, nextItem])
+        const nextItem = Math.max(1, items.length + 1);
+        setItems([...items, nextItem]);
     };
     const removeItem = () => {
-        const endRange = Math.max(0, items.length - 1)
-        setItems(items.slice(0, endRange))
+        const endRange = Math.max(0, items.length - 1);
+        setItems(items.slice(0, endRange));
     };
+    /***************************/
 
     const getProducts = async () => {
         const { data } = await listProducts();
@@ -56,16 +73,34 @@ function Main() {
     return (
         <div className="container">
         <Header className="header-main"></Header>
+        {/* 
+            <div>
+                <button onClick={addItem}> + </button>
+                <button onClick={removeItem}> - </button>
+            </div> */}
+
+        <div className="carousel-wrapper">
+            <ReactElasticCarousel breakPoints={breakPoints}>
+            {items.map((item) => (
+                // <img src={ProdutoModal1}/>
+                <Item key={item}>{item}</Item>
+            ))}
+            </ReactElasticCarousel>
+        </div>
 
         <section className="home-main" id="home">
             <div className="content">
-            <h3 className="titulo">Qual será a sua escolha?</h3>
-            <p className="subtitulo">
-            Você não precisa ir longe e nem esperar horas por uma comida, escolha uma opção pertinho de você
-            </p>
+            {/* <h3 className="titulo">Qual será a sua escolha?</h3> */}
+            {/* <p className="subtitulo">
+                Você não precisa ir longe e nem esperar horas por uma comida,
+                escolha uma opção pertinho de você
+            </p> */}
             </div>
         </section>
-        <h1 className="heading"> <span>Alguns</span> Produtos </h1>
+        <h1 className="heading">
+            {" "}
+            <span>Alguns</span> Produtos{" "}
+        </h1>
         <section className="menu" id="menu">
             <div className="box-container">
             <div className="box">
@@ -191,7 +226,7 @@ function Main() {
             </div>
             {/* { products.map(renderProduct) } */}
         </section>
-         <Review></Review> 
+        <Review></Review>
         <Footer></Footer>
         </div>
     );
